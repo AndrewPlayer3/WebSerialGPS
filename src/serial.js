a0567674b872cf3b21366b455fe4980771e73a18
Andrew Player <andrewplayer3@gmail.com>
2022-04-20 12:24:48 -0800

    more formatting and comments

--- src/serial.js
+++ src/serial.js
@@ -5,46 +5,60 @@
  *    Note: See the README for resources describing the relevant NMEA Sentence Standards.  
 /*/
 
+
 let reader, 
     gpsPort
 let keepReading = true,
     needConcat  = false,
-    showMap     = false
+    showMap     = false,
+    portIsOpen  = false
 let data_string = ''
 let data_arr    = []
 let decoder     = new TextDecoder('UTF-8')
 
 
-async function openGPSPort() {
-    try {
-        gpsPort = await navigator.serial.requestPort({
-            filters: [
-                {
-                    usbVendorId: 0x1546, // GPS USB Vendor ID, can be found by using lsusb
-                },                       // You may need to add your GPS's vid here ❗
-            ],
-        })
+/* Request access to the GPS's port, and open it so it can be read. */
+async function openGPSPort() 
+{
+    try 
+    {
+        gpsPort = await navigator.serial.requestPort(
+            {
+                filters: 
+                [
+                    {
+                        usbVendorId: 0x1546, // GPS USB Vendor ID, can be found by using lsusb
+                    },                       // You may need to add your GPS's vid here ❗
+                ],
+            }
+        )
         console.log(
             'Selected Port: ', gpsPort.getInfo()
         )
-        await gpsPort.open({
-            baudRate: 9600,
-            dataBits: 8,
-            endBits: 1,
-        })
+        await gpsPort.open(
+            {
+                baudRate: 9600,
+                dataBits: 8,
+                endBits: 1,
+            }
+        )
         console.log(
             'The selected port has been opened.'
         )
-    } catch (error) {
+        return true
+    } catch (error) 
+    {
         console.log(
             'Error Opening Port: ', error.message
         )
+        return false
     }
 }
 
 
-async function getMapURL(latitude, longitude) {
-
+/* Build the OpenStreetMap Embed URL to display the given location. */
+async function getMapURL(latitude, longitude) 
+{
     let north_bound = latitude + 0.0005
     let south_bound = latitude - 0.0005
     let east_bound  = longitude - 0.0005
@@ -63,11 +77,36 @@ async function getMapURL(latitude, longitude) {
 }
 
 
-async function readUntilClosed() {
-    while (gpsPort.readable && keepReading) {
+/* Parse an NMEA GPS Sentence to get a more displayable format. */ 
+async function parseNMEAMessage(data)
+{
+    if (data.startsWith('$GPGGA')) 
+    {
+        try 
+        {
+            let message = new GPGGA(data)
+            return message
+        } catch (error) 
+        {
+            console.log(
+                "Error Processing GPGGA Sentence: ", error.message
+            )
+            return null
+        }
+    }
+}
+
+
+/* Keep reading and displaying the GPS serial data until user clicks stop. */
+async function readUntilClosed() 
+{
+    while (gpsPort.readable && keepReading) 
+    {
         reader = gpsPort.readable.getReader()
-        try {
-            while (true) {
+        try 
+        {
+            while (true) 
+            {
                 const { value, done } = await reader.read()
                 
                 if (done) // "done" is when the user clicks stop,
@@ -78,38 +117,40 @@ async function readUntilClosed() {
                     : decoder.decode(value)
                 data_arr = temp_string.split('\r\n')
                 
-                if (!temp_string.endsWith('\r\n')) {
+                if (!temp_string.endsWith('\r\n')) 
+                {
                     needConcat = true
                     data_string += data_arr.pop()
-                } else {
+                } else 
+                {
                     needConcat = false
                     data_string = ''
                 }
                 
-                for (let j = 0; j < data_arr.length; j++) {
-                    let data = data_arr[j]
-                    if (data.startsWith('$GPGGA')) {
-                        try {
-                            let message = new GPGGA(data)
-                            if (message.latitude != 0 && message.longitude != 0) {
-                                document.querySelector('#dataField').innerText = message.toStringVerbose()
-                                if (showMap) 
-                                    await getMapURL(message.latitude, message.longitude)
-                            }
-                        } catch (error) {
-                            console.log(
-                                "Error Processing GPGGA Sentence: ", error.message
-                            )
-                        }
+                for (let j = 0; j < data_arr.length; j++) 
+                {
+                    let message = await parseNMEAMessage(data_arr[j])
+                    if (
+                        message &&
+                        message.type == 'GGA' &&
+                        message.latitude != 0 &&
+                        message.longitude != 0
+                    ){
+                        document.querySelector('#dataField')
+                            .innerText = message.toStringVerbose()
+                        if (showMap) 
+                            await getMapURL(message.latitude, message.longitude)
                     }
                 }
                 data_arr = []
             }
-        } catch (error) {
+        } catch (error) 
+        {
             console.log(
                 'Error in Reader Loop: ', error.message
             )
-        } finally {
+        } finally 
+        {
             reader.releaseLock()
         }
     }
@@ -117,42 +158,65 @@ async function readUntilClosed() {
 }
 
 
-document.addEventListener('DOMContentLoaded', async () => {
+/* Once the DOM is loaded, this creates the event listeners for the buttons. */
+document.addEventListener('DOMContentLoaded', 
+    async () => 
+    {
+        let connect_button    = document.querySelector('#connect')
+        let disconnect_button = document.querySelector('#disconnect')
+        let run_button        = document.querySelector('#run')
+        let map_button        = document.querySelector('#map-btn')
 
-    let connect_button    = document.querySelector('#connect')
-    let disconnect_button = document.querySelector('#disconnect')
-    let run_button        = document.querySelector('#run')
-    let map_button        = document.querySelector('#map-btn')
-
-    connect_button.addEventListener('click', async () => {
-        await openGPSPort()
-        run_button.setAttribute('style', 'visibility:visible')
-        document.querySelector('#dataField').innerText = 'Click run to begin.'
-    })
-
-    disconnect_button.addEventListener('click', async () => {
-        keepReading = false
-        await reader.cancel()
-        console.log(
-            'The serial port has been closed.'
+        connect_button.addEventListener('click', 
+            async () => 
+            {
+                if (await openGPSPort())
+                {
+                    run_button.setAttribute('style', 'visibility:visible')
+                    document.querySelector('#dataField')
+                        .innerText = 'Click run to begin.'
+                } else
+                {
+                    document.querySelector('#dataField')
+                        .innerText = 'Error opening port. Try again... :('
+                }
+            }
         )
-        run_button.setAttribute('style', 'visibility: hidden')
-        disconnect_button.setAttribute('style', 'visibility: hidden')
-    })
 
-    run_button.addEventListener('click', async () => {
-        disconnect_button.setAttribute('style', 'visibility: visible')
-        document.querySelector('#dataField').innerText = 'Data: Loading...'
-        readUntilClosed()
-    })
+        disconnect_button.addEventListener('click', 
+            async () => 
+            {
+                keepReading = false
+                await reader.cancel()
+                console.log(
+                    'The serial port has been closed.'
+                )
+                run_button.setAttribute('style', 'visibility: hidden')
+                disconnect_button.setAttribute('style', 'visibility: hidden')
+            }
+        )
 
-    map_button.addEventListener('click', async () => {
-        showMap = !showMap
-        visibility = showMap 
-            ? "visible"
-            : "hidden"
-        document.querySelector('#map').setAttribute('style', `visibility: ${visibility}`)
-    })
-})
+        run_button.addEventListener('click',
+            async () => 
+            {
+                disconnect_button.setAttribute('style', 'visibility: visible')
+                document.querySelector('#dataField').innerText = 'Data: Loading...'
+                readUntilClosed()
+            }
+        )
+
+        map_button.addEventListener('click',
+            async () => 
+            {
+                showMap    = !showMap
+                visibility =  showMap 
+                    ? "visible"
+                    : "hidden"
+                document.querySelector('#map')
+                    .setAttribute('style', `visibility: ${visibility}`)
+            }
+        )
+    }
+)
 
 
